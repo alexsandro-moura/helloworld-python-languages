301207f57f31455298a0fcb79d2f9d6f09ff017a
Alexsandro <alexsandro_moura@hotmail.com>
2022-03-24 19:04:03 -0300

    hello-world python

---
+++ hello-world.py
@@ -0,0 +1,86 @@
+#1. English = inglês#
+#2. Português = português#
+#3. Español = espanhol#
+#4. Deutsch = alemão#
+#5. Français = francês#
+#6. Italiano = italiano#
+#7. 日本 = japonês#
+#8. 中國人 = chinês#
+#9. 한국인 = coreano#
+#10. русский = russo#
+
+idioma_Escolhido = int(input("""
+Select your language:
+1. English
+2. Português
+3. Español
+4  Deutsch
+5. Français
+6. Italiano
+7. 日本
+8. 中國人
+9. 한국인
+10.русский\n
+"""))
+
+if idioma_Escolhido == 1:
+    print ("Hello World! My name is Alexsandro. I'm a software engineer and I'm 25 years old.")
+
+elif idioma_Escolhido == 2:
+    print ("Olá, mundo! Meu nome é Alexsandro. Sou engenheiro de software e tenho 25 anos")
+#1. English = inglês#
+#2. Português = português#
+#3. Español = espanhol#
+#4. Deutsch = alemão#
+#5. Français = francês#
+#6. Italiano = italiano#
+#7. 日本 = japonês#
+#8. 中國人 = chinês#
+#9. 한국인 = coreano#
+#10. русский = russo#
+
+idioma_Escolhido = int(input("""
+Select your language:\n
+1. English
+2. Português
+3. Español
+4  Deutsch
+5. Français
+6. Italiano
+7. 日本
+8. 中國人
+9. 한국인
+10. Pусский\n
+"""))
+
+if idioma_Escolhido == 1:
+    print ("Hello World! My name is Alexsandro. I'm a software engineer and I'm 25 years old.")
+
+elif idioma_Escolhido == 2:
+    print ("Olá, mundo! Meu nome é Alexsandro. Sou engenheiro de software e tenho 25 anos.")
+    
+elif idioma_Escolhido == 3:
+    print ("¡Hola mundo! Mi nombre es Alexsandro. Soy ingeniero de software y tengo 25 años.")
+    
+elif idioma_Escolhido == 4:
+    print ("Hallo Welt! Mein Name ist Alexandros. Ich bin Software-Ingenieur und 25 Jahre alt.")
+    
+elif idioma_Escolhido == 5:
+    print ("Bonjour le monde! Je m'appelle Alexsandro. Je suis ingénieur logiciel et j'ai 25 ans.")
+    
+elif idioma_Escolhido == 6:
+    print ("Ciao mondo! Mi chiamo Alessandro. Sono un ingegnere del software e ho 25 anni.")
+    
+elif idioma_Escolhido == 7:
+    print (' "こんにちは世界"私の名前はアレクサンドロです。私はソフトウェアエンジニアで、25歳です。')
+    #atenção especial no neste caso. No idioma japonês, as "aspas duplas" fazem...#
+    #...parte desta frase, interferindo assim na sintaxe do codigo. Por isso foram utilizadas 'aspas simples'#
+    
+elif idioma_Escolhido == 8:
+    print ("你好，世界！我的名字是亚历山德罗。我是一名软件工程师，今年 25 岁。") 
+    
+elif idioma_Escolhido == 9:
+    print ("안녕하세요 월드입니다! 제 이름은 알렉산드로입니다. 저는 소프트웨어 엔지니어이고 25세입니다.")
+    
+elif idioma_Escolhido == 10:
+    print ("Привет, мир! Меня зовут Александро. Я инженер-программист, мне 25 лет.")
